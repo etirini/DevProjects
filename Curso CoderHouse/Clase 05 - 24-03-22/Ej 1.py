6c67774ebc9b38ea3a021ab0499541f4b2a18334
etirini <etirini@gmail.com>
2022-03-25 07:25:55 -0300

    while for range

---
+++ Curso CoderHouse/Clase 05 - 24-03-22/Ej 1.py
@@ -0,0 +1,38 @@
+"""""
+contador = 0
+while contador < 10:
+    print(f"Hola, el contador esta en {contador}")
+    contador = contador + 1
+
+
+cuenta = 20 
+while cuenta >= 0:
+    print(f"Lanzamiento en {cuenta}")
+    cuenta = cuenta - 1 
+    continue
+else:
+    print("Lanzamiento correcto")
+"""
+
+alumno = ["fulano", "mengano", "sultano", "lontano"]
+
+"""for nombre in alumno:
+    print(f"el alumno se llama {nombre}")"""
+
+
+#for index, nombre in enumerate(alumno):
+#    print(f"el indice es {index}")
+#    print(f"el nombre es {nombre}")
+
+#numeros = range(10)
+#for numero in numeros:
+#    print(numero)
+
+
+#numeros = range(3, 30)
+#for numero in numeros:
+#    print(numero)
+
+numeros = range(5, 30, 5)
+for numero in numeros:
+    print(numero)

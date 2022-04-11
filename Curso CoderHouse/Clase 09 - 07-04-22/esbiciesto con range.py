3b54ec82f9af99be793cd2ad73c63ffd944cff76
etirini <etirini@gmail.com>
2022-04-11 07:34:31 -0300

    Ej8 Greedy

--- Curso CoderHouse/Clase 09 - 07-04-22/esbiciesto con range.py
+++ Curso CoderHouse/Clase 09 - 07-04-22/esbiciesto con range.py
@@ -2,10 +2,8 @@
 #La segunda hace el calculo y retorna el resultado
 
 def saludo():
-    #print("Por favor ingrese un anio a calcular")
     ingreso = anios()
     while ingreso <= 4000:
-    #print(ingreso)
         calcular(ingreso)
         print("el anio ", ingreso, calcular(ingreso))
         ingreso = ingreso +1
@@ -18,7 +16,7 @@ def calcular(ingreso):
         return "no es bisiesto"
 
 def anios():
-    for anio in range(1,4000):
+    for anio in range(0,4000):
         return(anio)
 
 saludo()

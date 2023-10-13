acb27ba97fa320c3576fb30040494f56a6914250
etirini <etirini@gmail.com>
2023-10-13 08:54:31 -0400

    Think Like a programmer excercises

---
+++ TLAP/001 - Stars.py
@@ -0,0 +1,10 @@
+for row in range(1,6):
+    for _ in range(row):
+        print("#", end="")
+    print("")
+for row in range(1,5):
+    for _ in range(5-row):
+        print("#", end="")
+    print("")
+
+ 
--- slotmachine/main.py
+++ slotmachine/main.py
@@ -1,8 +1,27 @@
+import random
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    
+
+
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit $?")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0 :
@@ -27,12 +46,33 @@ def get_number_of_lines():
             print("Please enter a number")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount needs to be between ${MIN_BET} - ${MAX_BET}" )
+        else:
+            print("Please enter a number")
+    return amount
+
 
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(f"Your balance of ${balance} is insufficient for that bet")
+        else:
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet equals to {total_bet}")
+
 
 
 

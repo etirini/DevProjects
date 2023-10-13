a5a3bacc433a5c7bffb8ba56fa2aae75c47f9764
etirini <etirini18@hotmail.com>
2023-10-13 17:53:41 -0400

    Add files via upload

--- TLAP/002 - Luhn.py
+++ TLAP/002 - Luhn.py
@@ -1,29 +1,33 @@
 def lun_val():
     user_value_digits = []
-    big_number = []
     user_value = input("Insert number to validate ")
     if user_value.isdigit():
         
         for index, digit in enumerate(user_value):
             digit = int(digit)
             if index % 2 == 1:
-                mul_number(digit)
                 digit *= 2
                 if digit > 9:
                     big_d=decomp_number(digit)
                     user_value_digits.append(big_d)
-
-            user_value_digits.append(digit)
-        print(user_value_digits)
-
-def mul_number(digit):
-
-
+                else:
+                    user_value_digits.append(digit)
+            else:
+                user_value_digits.append(digit)
+        digit_sum = sum(user_value_digits)
+        validate_sum(digit_sum)
 
 def decomp_number(digit):
    big_number = [int(d) for d in str(digit)]
    summ = sum(big_number)
-   print("summ=", summ)
    return summ
 
+def validate_sum(digit_sum):
+    if digit_sum % 10 == 0:
+        return print("The validation is successful")
+    else:
+        return print("The validation not successful")
+
+
 lun_val()
+
